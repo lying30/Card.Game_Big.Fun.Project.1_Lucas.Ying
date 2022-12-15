94f6c0e4447836f52eab87a883f5b9fe69a86712
Lucas_Ying <lucas.ying@menloschool.org>
2022-12-14 21:03:44 -0800

    Done!

--- src/Deck.java
+++ src/Deck.java
@@ -1,6 +1,6 @@
 /**
  * Author: Lucas Ying
- * Date: 12/07/22
+ * Date: 12/14/22
  */
 import java.util.ArrayList;
 import java.util.Random;
@@ -11,12 +11,15 @@ public class Deck {
 
     public Deck(String[] ranks, String[] suits, int[] values) {
         cards = new ArrayList<Card>();
+        //iterate through the suits and ranks and create the cards in the deck
         for(int i = 0; i < suits.length; i++) {
             for(int j = 0; j < ranks.length; j++) {
                 cards.add(new Card(ranks[j], suits[i], values[j]));
             }
         }
-        int cardsLeft = cards.size();
+        // initiate the cards left as the 52 cards or size of cards in the deck
+        cardsLeft = cards.size();
+        shuffle();
     }
     public boolean isEmpty() {
         if(cardsLeft == 0) {
@@ -24,19 +27,31 @@ public class Deck {
         }
         return false;
     }
-    public int getCardsLeft(int cardsLeft) {
+    public int getCardsLeft() {
         return cardsLeft;
     }
     public Card deal() {
+        //if there are zero cards return null
         if(cardsLeft == 0) {
             return null;
         }
+        //return the top card subtract and then return
         return cards.get(--cardsLeft);
-        //cardsLeft - 1 then return the cardsleft to return the top card subtract and then return
     }
+    //
     public void shuffle() {
+        //loops through starting from the top of the deck going down
         for(int i = cardsLeft-1; i > 0; i--) {
-
+            //creates a random number out of the number of cards left
+            int r = (int)(Math.random()*i);
+            //creates a variable that holds the top card
+            Card top = cards.get(i);
+            //creates a variable that holds the random card to be swapped
+            Card random = cards.get(r);
+            //swaps the random card with top card
+            cards.set(i, random);
+            cards.set(r, top);
         }
+        cardsLeft = cards.size();
     }
 }
